2d505ac100e00d27564b122497052a6ab9043ea2
Infinity <Infinity@DESKTOP-IE3B7NE>
2025-02-04 22:46:47 +0800

    添加项目文件。

---
+++ Random_FloatingTool/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Random_FloatingTool
+{
+
+    public partial class MainWindow : Window
+    {
+        public ToolBox toolBox;
+        public double startX, startY;
+        public bool isWindowToggled = false;
+        public double centerX;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Opacity = 0.8;
+
+            // 获取屏幕宽度
+            centerX = SystemParameters.PrimaryScreenWidth / 2;
+        }
+
+        private void ToggleToolBox()
+        {
+            if (toolBox == null)
+            {
+                toolBox = new ToolBox();
+                toolBox.Owner = this;
+                if (this.Left <= centerX)
+                {
+                    toolBox.Left = this.Left + this.Width + 20;
+                    toolBox.Top = this.Top;
+                }
+                else
+                {
+                    toolBox.Left = this.Left - toolBox.Width - 20;
+                    toolBox.Top = this.Top;
+                }
+                toolBox.Show();
+            }
+            else
+            {
+                toolBox.Visibility = toolBox.IsVisible
+                    ? Visibility.Hidden
+                    : Visibility.Visible;
+            }
+            isWindowToggled = !isWindowToggled;
+        }
+
+        private void Logo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startX = Window1.Left;
+            startY = Window1.Top;
+            ToggleToolBox();
+        }
+
+        private void Logo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+                if ((Math.Abs(Window1.Left - startX) > 10 || (Math.Abs(Window1.Top - startY) > 10)) && toolBox != null)
+                {
+                    ToggleToolBox();
+                }
+            }
+        }
+
+        private void MainWindow_LocationChanged(object sender, EventArgs e)
+        {
+            if (toolBox != null)
+            {
+
+                if (this.Left <= centerX)
+                {
+                    toolBox.Left = this.Left + this.Width + 20;
+                    toolBox.Top = this.Top;
+                }
+                else
+                {
+                    toolBox.Left = this.Left - toolBox.Width - 20;
+                    toolBox.Top = this.Top;
+                }
+            }
+        }
+    }
+}
---
+++ Random_FloatingTool/ToolBox.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Random_FloatingTool
+{
+    /// <summary>
+    /// ToolBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class ToolBox : Window
+    {
+        public ToolBox()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();  
+            Result.Text = random.Next(Convert.ToUInt16(Min.Text),Convert.ToUInt16(Max.Text)).ToString() + "被抽中了";
+        }
+
+        private void MinMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Min.Text = (Convert.ToUInt16(Min.Text) - 1).ToString();
+        }
+
+        private void MinPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Min.Text = (Convert.ToUInt16(Min.Text) + 1).ToString();
+        }
+
+        private void MaxMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Max.Text = (Convert.ToUInt16(Max.Text) - 1).ToString();
+        }
+
+        private void MaxPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Max.Text = (Convert.ToUInt16(Max.Text) + 1).ToString();
+        }
+    }
+}
